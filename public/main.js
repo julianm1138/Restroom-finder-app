7491214a09207ffafd17bd3932e96e3bb4e254da
Jasmine <105261487+codewithjazzy@users.noreply.github.com>
2024-01-10 03:48:09 -0800

    working database. worldwide locations added

--- public/main.js
+++ public/main.js
@@ -1,41 +1,49 @@
-const update = document.querySelector('#update-button')
-const deleteButton = document.querySelector('#delete-button')
-const messageDiv = document.querySelector('#message')
+const upvoteBtns = document.querySelectorAll('.fa-thumbs-up')
 
-update.addEventListener('click', _ => {
-  fetch('/restrooms', {
-    method: 'put',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      name: 'Darth Vadar',
-      restroom: 'I find your lack of faith disturbing.'
-    })
+upvoteBtns.forEach(button => {
+  button.addEventListener('click', () => {
+      // get the Id from the parent <li> element
+      const restroomId = button.parentElement.id;
+      console.log("Upvoting restroom with _id:", restroomId);
+
+      fetch('/voteUp', {
+          method: 'put',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ '_id': restroomId })
+      })
+      .then(res => {
+          if (res.ok) return res.json();
+      })
+      .then(response => {
+          window.location.reload(true);
+      })
+      .catch(error => console.error(error));
   })
-    .then(res => {
-      if (res.ok) return res.json()
-    })
-    .then(response => {
-      window.location.reload(true)
-    })
 })
 
-deleteButton.addEventListener('click', _ => {
-  fetch('/restrooms', {
-    method: 'delete',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      name: 'Darth Vadar'
-    })
-  })
-    .then(res => {
-      if (res.ok) return res.json()
-    })
-    .then(response => {
-      if (response === 'No restroom to delete') {
-        messageDiv.textContent = 'No Darth Vadar restroom to delete'
-      } else {
-        window.location.reload(true)
-      }
-    })
-    .catch(console.error)
-})
+   
+
+
+// const deleteButton = document.querySelector('#delete-button')
+// const messageDiv = document.querySelector('#message')
+
+// deleteButton.addEventListener('click', _ => {
+//   fetch('/restrooms', {
+//     method: 'delete',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ 
+//       name: 'Darth Vadar'
+//     })
+//   })
+//     .then(res => {
+//       if (res.ok) return res.json()
+//     })
+//     .then(response => {
+//       if (response === 'No restroom to delete') {
+//         messageDiv.textContent = 'No Darth Vadar restroom to delete'
+//       } else {
+//         window.location.reload(true)
+//       }
+//     })
+//     .catch(console.error)
+// })
